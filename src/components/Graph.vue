f4160f23e0422ae023845c3a10c900b80bb19dae
Mahima Ramgolam <mahima7691487@gmail.com>
2021-12-19 22:27:49 +0400

    graph working yay!

--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -1,11 +1,11 @@
 <template>
   <Menu />
   <div class="bg-yellow-500 p-4 text-center">In progress..</div>
-  <div class="bg-blue-500 p-4 text-center">{{ dates }}</div>
-  <div class="bg-blue-500 p-4 text-center">{{ deathValues }}</div>
   <div>
     <canvas id="covid-chart" height="100"></canvas>
   </div>
+  <input type="range" min="0" max="200" v-model="myRange">
+  {{ myRange}}
 </template>
 
 <script>
@@ -28,6 +28,8 @@ export default {
       dates: [],
       deathValues: [],
       number: 2,
+      covidChartData: [],
+      myRange: 50
     };
   },
 
@@ -56,15 +58,16 @@ export default {
       if (this.countries.length === 0) {
         return [];
       }
-      return this.countries["Mauritius"].filter((item, index) => {
+      return this.countries["Mauritius"].map((item, index) => {
         return item.deaths;
       });
     },
+
     datesForOneCountry() {
       if (this.countries.length === 0) {
         return [];
       }
-      return this.countries["Mauritius"].filter((item, index) => {
+      return this.countries["Mauritius"].map((item, index) => {
         return item.date;
       });
     },
@@ -77,55 +80,62 @@ export default {
         this.countries = data;
 
         this.countries["Mauritius"].filter((item, index) => {
-          if (index < 30) {
+          if (index < 200) {
             this.dates.push(item.date);
             this.deathValues.push(item.deaths);
           }
         });
-
+        this.setCovidChartData(this.datesForOneCountry, this.deathValuessForOneCountry);
         this.loader = false;
       });
     console.log("dates", this.dates);
     console.log("deaths", this.deathValues);
 
-    const covidChartData = {
-      type: "line",
-      data: {
-        labels: this.dates.slice(0, 30),
-        datasets: [
-          {
-            label: "Number of Moons",
-            data: this.deathValues.slice(0, 30),
-            backgroundColor: "rgba(54,73,93,.5)",
-            borderColor: "#36495d",
-            borderWidth: 3,
-          },
-          //   {
-          //     label: "covidary Mass (relative to the Sun x 10^-6)",
-          //     data: [0.166, 2.081, 3.003, 0.323, 954.792, 285.886, 43.662, 51.514],
-          //     backgroundColor: "rgba(71, 183,132,.5)",
-          //     borderColor: "#47b784",
-          //     borderWidth: 3
-          //   }
-        ],
-      },
-      options: {
-        responsive: true,
-        lineTension: 1,
-        scales: {
-          yAxes: [
-            {
-              ticks: {
-                beginAtZero: true,
-                padding: 25,
+    
+  },
+
+  methods: {
+    setCovidChartData(dates, deathValues) {
+      this.covidChartData = {
+            type: "line",
+            data: {
+              labels: dates,
+              datasets: [
+                {
+                  label: "Number of deaths",
+                  data: deathValues,
+                  backgroundColor: "rgba(54,73,93,.5)",
+                  borderColor: "#36495d",
+                  borderWidth: 3,
+                },
+                //   {
+                //     label: "covidary Mass (relative to the Sun x 10^-6)",
+                //     data: [0.166, 2.081, 3.003, 0.323, 954.792, 285.886, 43.662, 51.514],
+                //     backgroundColor: "rgba(71, 183,132,.5)",
+                //     borderColor: "#47b784",
+                //     borderWidth: 3
+                //   }
+              ],
+            },
+            options: {
+              responsive: true,
+              lineTension: 1,
+              scales: {
+                yAxes: [
+                  {
+                    ticks: {
+                      beginAtZero: true,
+                      padding: 25,
+                    },
+                  },
+                ],
               },
             },
-          ],
-        },
-      },
-    };
-    const ctx = document.getElementById("covid-chart");
-    new Chart(ctx, covidChartData);
-  },
+          };
+      const ctx = document.getElementById("covid-chart");
+      new Chart(ctx, this.covidChartData);
+    }
+    
+  }
 };
 </script>
