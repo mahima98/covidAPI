afe2ece4db33972d3a5804256d6005a982bed84a
Mahima Ramgolam <mahima7691487@gmail.com>
2022-04-24 22:14:36 +0400

    updated covidAPI

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -2,22 +2,8 @@
   <div class="m-4 shadow-xl rounded-xl">
     <div class="bg-bluegrey-900 text-white">
       <div class="grid grid-cols-5 text-white">
-        <router-link to="/" class="hover:bg-bluegrey-800 px-10 py-5"
-          >Hw</router-link
-        >
-        <router-link to="/animals" class="hover:bg-bluegrey-800 px-10 py-5"
-          >Animals</router-link
-        >
-        <router-link to="/animals/dogs" class="hover:bg-bluegrey-800 px-10 py-5"
-          >Dogs</router-link
-        >
-        <router-link
-          to="/covid/Mauritius"
-          class="hover:bg-bluegrey-800 px-10 py-5"
-          >Covid (Mauritius | Bahamas)</router-link
-        >
         <router-link to="/countries" class="hover:bg-bluegrey-800 px-10 py-5"
-          >Countries</router-link
+          >Display Countries</router-link
         >
       </div>
     </div>
